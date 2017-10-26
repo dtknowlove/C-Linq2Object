81083f61f4a6f3634774c738184746a81800ba68
liuzhenhua <liuzhenhua@putao.com>
2017-10-26 19:35:07 +0800

    0.0.1 Console go through.

---
+++ Linq2Object/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Linq2Object
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Start a console project!");
+        }
+    }
+}
